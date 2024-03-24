56ef7fd80a61ba335920f9230d74284da209159c
GCAKuma <charithashen529@gmail.com>
2024-03-25 00:12:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace school
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(2, "abc");
+
+            Console.WriteLine(student1.id);
+
+            student1.crazy(student1.name);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace school
+{
+    internal class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public Student(int aid, string aname)
+        {
+            id = aid;
+            name = aname;
+        }
+
+        public void crazy(string aname)
+        {
+            Console.WriteLine("welcome  " + aname);
+        }
+    }
+
+    
+}
